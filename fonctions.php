d24a9f9a47311f66517a2677f7454ea99dccb6ad
unknown <nemorluther@gmail.com>
2021-01-15 12:08:48 +0100

    changement

---
+++ fonctions.php
@@ -0,0 +1,14 @@
+<?php
+
+    /*
+        - créer un namespace (première lettre du prénom + nom | ex.: dhurtrel)
+        - créer une fonction checkPassword permettant de vérifier la composition d'un mot de passe :
+            - 8 caractères
+            - 1 caractère spécial
+            - 1 minuscule
+            - 1 majuscule
+            - 1 chiffre
+        - afficher un message 
+    */
+
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    require_once('fonctions.php');
+?>
+
+<!DOCTYPE html>
+
+<html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP - 05 - namespace - eval</title>
+    </head>
+
+    <body>
+        
+        <?php
+            /*
+                - tester un mot de passe
+            */
+        ?>
+
+    </body>
+
+</html>
